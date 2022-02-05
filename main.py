c7d5137d4c68ff061eb04a525de5f20f2154d3c0
alfmorais <alfredneto@hotmail.com>
2022-02-05 20:46:37 -0300

    add o endpoint /createposts

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+
 
 app = FastAPI()
 
@@ -25,3 +27,26 @@ def get_posts():
         dict: Information regarding posts
     """
     return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    """
+    Request for the POST method.
+    URL = "/createposts"
+
+    Args:
+        payload (dict, optional): JSON body to send for API
+
+    Returns:
+        dict: Succes message
+    """
+    title = payload["title"]
+    content = payload["content"]
+    
+    new_post = {
+        "new_post": f"title: {title}",
+        "content": f"content: {content}",
+    }
+    
+    return new_post
