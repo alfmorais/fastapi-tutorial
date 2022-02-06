50b8447b599660d4d9dc8014b57bbb813ea798e3
alfmorais <alfredneto@hotmail.com>
2022-02-06 15:26:04 -0300

    uso do pydantic

--- main.py
+++ main.py
@@ -1,52 +1,29 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    """ 
-    Request for the GET method.
-    URL = "/"
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
-    Returns:
-        dict: Information regarding "hello world"
-    """
+
+@app.get("/")
+def hello_world():
     return {"message": "Welcome to My API"}
 
 
 @app.get("/posts")
 def get_posts():
-    """
-    Request for the GET method.
-    URL = "/posts"
-
-    Returns:
-        dict: Information regarding posts
-    """
     return {"data": "This is your posts"}
 
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    """
-    Request for the POST method.
-    URL = "/createposts"
-
-    Args:
-        payload (dict, optional): JSON body to send for API
-
-    Returns:
-        dict: Succes message
-    """
-    title = payload["title"]
-    content = payload["content"]
-    
-    new_post = {
-        "new_post": f"title: {title}",
-        "content": f"content: {content}",
-    }
-    
-    return new_post
+@app.post("/posts")
+def create_posts(post: Post):
+    return {"data": post}
